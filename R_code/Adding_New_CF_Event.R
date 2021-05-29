c17b74a5c85fa1bb555c79151703d7bebfc3cb94
voremargot <margot.vore@outlook.com>
2021-05-28 17:13:54 -0700

    Corrected error in uploading data to database

--- R_code/Adding_New_CF_Event.R
+++ R_code/Adding_New_CF_Event.R
@@ -22,10 +22,6 @@ CF_Summary=data.frame(); Sensor_Summary=data.frame(); Events_added= data.frame()
 Num= 1
 for (x in c(1:nrow(New_Events))){
   CF_File= 'working_directory/NewCF.xlsx'
-  if (New_Events[x,'name']== 'WTS844-20210401-Mid-1.xlsx' | New_Events[x,'name']== 'WTS844-20210401-Mid-2.xlsx'  |
-      New_Events[x,'name']=='WTS626-20210401-Mid-1.xlsx' | New_Events[x,'name']== 'WTS626-20210401-Mid-2.xlsx'){
-    next()
-  }
   EA= data.frame(name= New_Events[x,'name'],Googleid=New_Events[x,'id'], added= Sys.Date(), Num= Num)
   Events_added= rbind(Events_added,EA)
   
@@ -133,31 +129,31 @@ for (x in c(1:nrow(New_Events))){
   Num=Num+1
 }
 
-# CF_Summary_cut=  subset(CF_Summary,select=-c(Num))
-# 
-# Uni=  unique(CF_Summary_cut)
-# for (x in c(1:nrow(Uni))){
-#   R= CF_Summary_cut[which(CF_Summary_cut$SiteID==Uni[x,'SiteID']& CF_Summary_cut$Date==Uni[x,'Date'] & CF_Summary_cut$PMP==Uni[x,'PMP']& CF_Summary_cut$Loc==Uni[x,'Loc'] & CF_Summary_cut$PeriodID==Uni[x,'PeriodID']),]
-#   Uni[x,'Trials']=nrow(R)
-# }
-# 
-# for (r in c(1:nrow(Uni))){
-#   query <- sprintf("INSERT INTO chrl.calibration_events (PeriodID, SiteID, Date, PMP, Trial, Location) VALUES (%s,%s,'%s','%s',%s,'%s')",
-#                    Uni[r,'PeriodID'],
-#                    Uni[r,"SiteID"],
-#                    Uni[r,"Date"],
-#                    Uni[r,"PMP"],
-#                    Uni[r,"Trials"],
-#                    Uni[r,"Loc"])
-#   query <- gsub("\\n\\s+", " ", query)
-#   dbSendQuery(con, query)
-# }
+CF_Summary_cut=  subset(CF_Summary,select=-c(Num,Link))
+
+Uni=  unique(CF_Summary_cut)
+for (x in c(1:nrow(Uni))){
+  R= CF_Summary_cut[which(CF_Summary_cut$SiteID==Uni[x,'SiteID']& CF_Summary_cut$Date==Uni[x,'Date'] & CF_Summary_cut$PMP==Uni[x,'PMP']& CF_Summary_cut$Loc==Uni[x,'Loc'] & CF_Summary_cut$PeriodID==Uni[x,'PeriodID']),]
+  Uni[x,'Trials']=nrow(R)
+}
+
+for (r in c(1:nrow(Uni))){
+  query <- sprintf("INSERT INTO chrl.calibration_events (PeriodID, SiteID, Date, PMP, Trial, Location) VALUES (%s,%s,'%s','%s',%s,'%s')",
+                   Uni[r,'PeriodID'],
+                   Uni[r,"SiteID"],
+                   Uni[r,"Date"],
+                   Uni[r,"PMP"],
+                   Uni[r,"Trials"],
+                   Uni[r,"Loc"])
+  query <- gsub("\\n\\s+", " ", query)
+  dbSendQuery(con, query)
+}
 
 for (x in c(1:nrow(CF_Summary))){
-  query= sprintf("SELECT CalEventID from chrl.calibration_events WHERE SiteID=%s AND Date='%s' AND PMP='%s' AND Location='%s'", 
+  query= sprintf("SELECT CalEventID from chrl.calibration_events WHERE SiteID=%s AND Date='%s' AND PMP='%s' AND Location='%s'",
                  CF_Summary[x,'SiteID'], CF_Summary[x,'Date'],CF_Summary[x,'PMP'],CF_Summary[x,'Loc'])
   CF_Summary[x,'CalEventID'] <- dbGetQuery(con, query)
- 
+
 }
 
 for (R in c(1:nrow(Sensor_Summary))){
@@ -171,43 +167,57 @@ for (R in c(1:nrow(Sensor_Summary))){
   SensorID= dbGetQuery(con, query)
   if (nrow(SensorID)==0){
     SensorID= NA
+  } else if (nrow(SensorID)> 1){
+    SensorID= NA
   }
   Sensor_Summary[R,'SensorID']=SensorID
 }
 
 
 
-# for (x in c(1:nrow(Events_added))){
-#   Number= Events_added[R,'Num']
-#   query= sprintf("INSERT INTO chrl.googledriveid (file_name,driveid,date_added,caleventid) VALUES ('%s','%s','%s',%s)",
-#             Events_added[R,'name'],
-#             Events_added[R,'id'],
-#             Events_added[R,'added'],
-#             CF_Summary[which(CF_Summary$Num==Number),'CalEventID'])
-#   query <- gsub("\\n\\s+", " ", query)
-#   dbSendQuery(con, query)
-# }
+for (R in c(1:nrow(Events_added))){
+  Number= Events_added[R,'Num']
+  query= sprintf("INSERT INTO chrl.googledriveid (file_name,driveid,date_added,caleventid) VALUES ('%s','%s','%s',%s)",
+            Events_added[R,'name'],
+            Events_added[R,'id'],
+            Events_added[R,'added'],
+            CF_Summary[which(CF_Summary$Num==Number),'CalEventID'])
+  query <- gsub("\\n\\s+", " ", query)
+  dbSendQuery(con, query)
+}
 
-# for (x in c(1:nrow(Sensor_Summary))){
-#   Number= Sensor_Summary[R,'Num']
-#   query= sprintf("Insert INTO chrl.calibration_results ('CalEventID','SiteID','SensorID','trial_number','Temp','CF_value','Per_Err','Flags','Notes','Link') 
-#                  VALUES (%s,%s,%s,%s,%s,%s,%s,'%s','%s','%s')",
-#                  CF_Summary[which(CF_Summary$Num==Number),'CalEventID'],
-#                  CF_Summary[which(CF_Summary$Num==Number),'SiteID'],
-#                  Sensor_Summary[R,'SensorID'],
-#                  Sensor_Summery[],
-#                  Sensor_Summary[R,'Temp'],
-#                  Sensor_Summary[R,'CF'],
-#                  Sensor_Summary[R,'Err'],
-#                  Sensor_Summary[R,'Flags'],
-#                  Sensor_Summary[R,'Notes'],
-#                  sprintf("<a href=%s>%s</a>",CF_Summary[which(CF_Summary$Num==Number),'Link'],Events_added[which(Events_added$Num==Number),'name'])
-#   )
-#   query <- gsub("\\n\\s+", " ", query)
-#   query <- gsub('NA',"NULL", query)
-#   query <- gsub("'NULL'","NULL",query)
-#   dbSendQuery(con, query)
-# }
+Trial_assignment=CF_Summary[,c('Num','CalEventID')]
+Ca= c(0)
+for (f in c(1:nrow(Trial_assignment))){
+  ID= Trial_assignment[f,'CalEventID'] 
+  assign= sum(Ca==ID)+1
+  Trial_assignment[f,'TrialNum']=assign
+  Ca=append(Ca,ID)
+}
+
+for (R in c(1:nrow(Sensor_Summary))){
+  Number= Sensor_Summary[R,'Num']
+  if (is.null(Sensor_Summary[R,'Flags'])==TRUE){
+    Sensor_Summary[R,'Flags']=NA
+  }
+  query= sprintf("Insert INTO chrl.calibration_results (CalEventID,SiteID,SensorID,trial_number,Temp,CF_value,Per_Err,Flags,Notes,Link)
+                 VALUES (%s,%s,%s,%s,%s,%s,%s,'%s','%s','%s')",
+                 CF_Summary[which(CF_Summary$Num==Number),'CalEventID'],
+                 CF_Summary[which(CF_Summary$Num==Number),'SiteID'],
+                 Sensor_Summary[R,'SensorID'],
+                 Trial_assignment[which(Trial_assignment$Num==Number),'TrialNum'],
+                 Sensor_Summary[R,'Temp'],
+                 Sensor_Summary[R,'CF'],
+                 Sensor_Summary[R,'Err'],
+                 Sensor_Summary[R,'Flags'],
+                 Sensor_Summary[R,'Notes'],
+                 sprintf("<a href=%s>%s</a>",CF_Summary[which(CF_Summary$Num==Number),'Link'],Events_added[which(Events_added$Num==Number),'name'])
+  )
+  query <- gsub("\\n\\s+", " ", query)
+  query <- gsub('NA',"NULL", query)
+  query <- gsub("'NULL'","NULL",query)
+  dbSendQuery(con, query)
+}
 
 
 
