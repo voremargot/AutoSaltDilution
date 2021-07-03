55449e71ee9c1c4ea850a42b87dd3990961993e1
voremargot <margot.vore@outlook.com>
2021-07-03 10:20:07 -0700

    Fixed formatting

--- R_code/Add_New_Events.R
+++ R_code/Add_New_Events.R
@@ -1099,46 +1099,33 @@ for (S in Stations){
   Discharge_Results$QP <- Discharge_Results$Discharge+Discharge_Results$AbsErr
   Discharge_Results$QM <- Discharge_Results$Discharge-Discharge_Results$AbsErr
   
+  
+  
   # Determine the number for flags that were recorded for each salt wave
-for (R in c(1:nrow(Discharge_Results))){
-    SID <- Discharge_Results[R,'SensorID']
-    if (length(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'])==0){
-      Discharge_Results[R,'Flags'] <- NA
-      Discharge_Results[R,'Flag_count'] <- NA
-      Discharge_Results[R,'SD'] <- 'N'
-    } else {
-      Discharge_Results[R,'Flags'] <- EC_curve_results[EC_curve_results$SensorID==SID,'Comment']
-      FG= unlist(strsplit(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'], ","))
-      if (is.na(FG)==TRUE){
-        Discharge_Results[R,'Flag_count']=0
-      } else {
-        Discharge_Results[R,'Flag_count'] <- length(FG)
-      }
-      
-      
-      if ('Sd' %in% FG){
-        Discharge_Results[R,'SD'] <- 'Y'
-      } else {
+  for (R in c(1:nrow(Discharge_Results))){
+      SID <- Discharge_Results[R,'SensorID']
+      if (length(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'])==0){
+        Discharge_Results[R,'Flags'] <- NA
+        Discharge_Results[R,'Flag_count'] <- NA
         Discharge_Results[R,'SD'] <- 'N'
+      } else {
+        Discharge_Results[R,'Flags'] <- EC_curve_results[EC_curve_results$SensorID==SID,'Comment']
+        FG= unlist(strsplit(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'], ","))
+        if (is.na(FG)==TRUE){
+          Discharge_Results[R,'Flag_count']=0
+        } else {
+          Discharge_Results[R,'Flag_count'] <- length(FG)
+        }
+        
+        
+        if ('Sd' %in% FG){
+          Discharge_Results[R,'SD'] <- 'Y'
+        } else {
+          Discharge_Results[R,'SD'] <- 'N'
+        }
       }
-    }
   }
-
-
-
-
-
-
-#  for (R in c(1:nrow(Discharge_Results))){
- #   SID <- Discharge_Results[R,'SensorID']
-  #  if (length(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'])==0){
-   #   Discharge_Results[R,'Flags'] <- NA
-    #  Discharge_Results[R,'Flag_count'] <- NA
-    #} else {
-     # Discharge_Results[R,'Flags'] <- EC_curve_results[EC_curve_results$SensorID==SID,'Comment']
-      #Discharge_Results[R,'Flag_count'] <- length(unlist(strsplit(EC_curve_results[EC_curve_results$SensorID==SID,'Comment'], ",")))
-   # }
- # }
+  
   
   ###########################################
   # Look for timing offset between salt waves
@@ -1188,26 +1175,13 @@ for (R in c(1:nrow(Discharge_Results))){
   # Flag which discharge values are part of the average discharge calculation
   Discharge_Results[(Discharge_Results$Flag_count<2 & Discharge_Results$SD=='N'),'Used'] <- 'Y'
   Discharge_Results[!(Discharge_Results$Flag_count<2 & Discharge_Results$SD=='N'),'Used'] <- 'N'
-
-
-
-
- # Max_Q <- max(Discharge_Results[Discharge_Results$Flag_count <2, 'QP'],na.rm=TRUE)
- # Min_Q <- min(Discharge_Results[Discharge_Results$Flag_count <2, 'QM'],na.rm=TRUE)
-  
- # Average_Discharge <- mean(Discharge_Results[Discharge_Results$Flag_count<2, 'Discharge'], na.rm=TRUE)
- # TotalUncert <-  max(((Max_Q-Average_Discharge)/Average_Discharge*100),((Average_Discharge-Min_Q)/Average_Discharge*100))  
-  
-  # Flag which discharge values are part of the average discharge calculation
- # Discharge_Results[Discharge_Results$Flag_count<2,'Used'] <- 'Y'
- # Discharge_Results[!(Discharge_Results$Flag_count<2),'Used'] <- 'N'
   
   # Determine the mixing
   Mixing <- AutoSalt_Mixing(Discharge_Results[which(Discharge_Results$Used=='Y'),])
 
   
   ##########################################
-  # Summerizing the autosalt discharge event
+  # Summarizing the autosalt discharge event
   ##########################################
   if (is.na(Average_Discharge)==TRUE | is.null(Average_Discharge)==TRUE){
     DS <- data.frame(EventID=Event_Num,
