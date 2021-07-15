e92d2d08b9efaf20e1d2bc90a44debfecd7d2647
voremargot <margot.vore@outlook.com>
2021-07-15 11:09:08 -0700

    Made the script more user friendly and automated

--- R_code/Adding_rating_curve_to_database.R
+++ R_code/Adding_rating_curve_to_database.R
@@ -3,13 +3,14 @@
 # May 2021
 # 
 # This code is used to record which discharge events have been included in a given rating curve. 
-# After the rating curve is created and the CSV file has been updated with all the relavent information about 
+# After the rating curve is created and the CSV file has been updated with all the relevant information about 
 # the curve, the CSV file  will be read into the database, documenting which events were included and the 
-# rain event number that was assigned to the event during the RC process.
+# rain event number assigned to the each event during the RC process.
 #
 # This code enters data into the following database tables:
 # RC autosalt
 # RC Manual
+# RC Summary
 #
 # Abbreviations:
 # EC --> Electrical Conductivity
@@ -18,44 +19,90 @@
 
 
 ##-----------------------------------------------------------------------------------------------
-## ---------------------------Setting up the workspace------------------------------------------
+## ---------------------------Setting up the work space------------------------------------------
 ##-----------------------------------------------------------------------------------------------
-readRenviron('C:/Program Files/R/R-3.6.2/.Renviron')
-Path_RCMetadata="C:/Users/margo.DESKTOP-T66VM01/Desktop/VIU/GitHub/R_code/working_directory/Metadata_RC_626_V4.csv"
-
+readRenviron('C:/Program Files/R/R-4.1.0/.Renviron')
+Path_RCMetadata="C:/Users/margo.DESKTOP-T66VM01/Desktop/VIU/GitHub/R_code/working_directory/Metadata_RC_626_V4_updated.csv"
+options(warn = - 1) 
 
 options(java.parameters = "-Xmx8g")
-gg=gc()
 
 library(DBI)
 library(curl)
 library(tidyverse)
+library(googledrive)
 
 con <- dbConnect(RPostgres::Postgres(), dbname=Sys.getenv('dbname'),host=Sys.getenv('host'),user=Sys.getenv('user'),password=Sys.getenv('password'))
+drive_auth(email=Sys.getenv('email_gdrive'))
+##-------------------------------------------------------------------------------------------------
+##--------------------- Extracting relevant data from CSV file-------------------------------------
+##-------------------------------------------------------------------------------------------------
+print("Before running this code, upload your final RC documents to the appropriate google drive folder.")
+print("Make sure the site number is in the name of the RC documents you have uploaded to google drive.")
+ready = readline(prompt="Are your final documents in google drive (y/n)? ")
+if (ready='n' |ready='N' |ready='No'|ready='no'){
+  print("Please upload your documents correctly to google drive and then run the code again")
+  stop()
+}
 
-##-------------------------------------------------------------------------------------------------
-##--------------------- Extracting relavent data from CSV file-------------------------------------
-##-------------------------------------------------------------------------------------------------
-{Site=as.numeric(readline(prompt='SiteID that the new rating curve is for: '))}
+Site=as.numeric(readline(prompt='SiteID that the new rating curve is for: '))
+while (is.na(Site)==TRUE){
+  print("You didn't enter a site! Please try again")
+  Site=as.numeric(readline(prompt='SiteID that the new rating curve is for: '))
+}
 query= sprintf("SELECT * FROM chrl.RC_summary WHERE SiteID=%s",Site)
 Current_RC_version= dbGetQuery(con, query)
 while (nrow(Current_RC_version)==0){
   print("The SiteID you entered was is not valid. Please enter a different value.")
-  {Site=as.numeric(readline(prompt='SiteID that the new rating curve is for: '))}
+  Site=as.numeric(readline(prompt='SiteID that the new rating curve is for: '))
   query= sprintf("SELECT * FROM chrl.RC_summary WHERE SiteID=%s",Site)
   Current_RC_version= dbGetQuery(con, query)
+}
+Shift= readline(prompt='Was the a shift in the curve from the previous RC version (y/n):')
+if (Shift=='n' | Shift=='No' | Shift=='no'){
+  Shift='N'
+}
+if (Shift=='y' | Shift=='Yes' | Shift=='yes'){
+  Shift='Y'
 }
 
 RC= read.csv(Path_RCMetadata)
 
 Version= max(Current_RC_version$version,na.rm=TRUE)+1
-Max_Date= max(as.Date(RC$Date))
-Min_Date= min(as.Date(RC$Date))
+Max_Date= max(as.Date(RC$Date,"%m/%d/%Y"))
+Min_Date= min(as.Date(RC$Date,"%m/%d/%Y"))
 
-Query= sprintf("INSERT INTO chrl.RC_Summary (SiteID,Version,Start_Date,End_Date) VALUES(%s,%s,'%s','%s')",
-        Site,Version,Max_Date,Min_Date)
+drive= drive_ls(path=sprintf("AutoSalt_Hakai_Project/Rating_Curve/Plotting_and_metadata/Version %s rating curves",Version))
+if (nrow(drive)==0){
+  print(sprintf("There are no rating curve documents in version %s folder! Please upload the new rating curve documents to google drive and rerun the code",Version))
+  stop()
+}
+RC_doc= drive[grepl(as.character(Site),drive$name),]
+if (nrow(RC_doc)==0){
+  print(sprintf('There are no files in Version %s rating curve folder with %s in the file  name.',Version, Site))
+  print(sprintf("Please either rename the file so it has the site name in it OR upload the files associated with site %s",Site))   
+  stop()
+}
+
+
+Links= drive_link(RC_doc)
+Link1=sprintf("<a href= %s>%s</a>",Links[1],RC_doc[1,'name'])
+if (length(Links)>1){
+  Link2=sprintf("<a href= %s>%s</a>",Links[2],RC_doc[2,'name'])
+} else {
+  Link2= NA
+}
+
+
+Query= sprintf("INSERT INTO chrl.rc_summary (SiteID,Version,Start_Date,End_Date,Shift, Link1,Link2) VALUES(%s,%s,'%s','%s','%s','%s','%s')",
+        Site,Version,Min_Date,Max_Date,Shift,Link1,Link2)
+Query <- gsub("\\n\\s+", " ", Query)
+Query <- gsub('NA',"NULL", Query)
+Query <- gsub("'NULL'","NULL",Query)
 dbSendQuery(con,Query)
 
+query= sprintf("SELECT * FROM chrl.RC_summary WHERE SiteID=%s",Site)
+Current_RC_version= dbGetQuery(con, query)
 RCID= Current_RC_version[which(Current_RC_version$version==Version),"rcid"]
 
 ##-----------------------------------------------------------------------------------------------
@@ -90,15 +137,18 @@ for (r in c(1:nrow(AS_DF))){
                 AS_DF[r,'SiteID'],AS_DF[r,'EventID'],AS_DF[r,'RCID'],AS_DF[r,'EventNo']))
   query <- gsub("\\n\\s+", " ", query)
   query <- gsub('NA',"NULL", query)
-  # dbSendQuery(con, query)
+  dbSendQuery(con, query)
 }
 
 for (r in c(1:nrow(M_DF))){
   query= (sprintf('INSERT INTO chrl.rcmanual (SiteID,MDisID,RCID,EventNo) VALUES (%s,%s,%s,%s)',
-                  M_DF[r,'SiteID'],M_DF[r,'MDisID'],AS_DF[r,'RCID'],AS_DF[r,'EventNo']))
+                  M_DF[r,'SiteID'],M_DF[r,'MDisID'],M_DF[r,'RCID'],M_DF[r,'EventNo']))
   query <- gsub("\\n\\s+", " ", query)
   query <- gsub('NA',"NULL", query)
-  # dbSendQuery(con, query)
+  dbSendQuery(con, query)
 }
+
+dbDisconnect(con)
+options(warn = 0)
 
 
